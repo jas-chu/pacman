b166cd7e6824f56f169225a05272405947f62963
jaschu <jaschu@jaschu-VPCYB15AL>
2014-09-18 15:23:02 -0300

    Limpieza de comentarios y formato para entrega de test de configuracion

--- 7510201402g1/src/test/java/com/tdd/configuracion/ConfiguracionTest.java
+++ 7510201402g1/src/test/java/com/tdd/configuracion/ConfiguracionTest.java
@@ -1,28 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.tdd.configuracion;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author agu
- */
 public class ConfiguracionTest {
-    
+
     public ConfiguracionTest() {
     }
 
-
-
-    /**
-     * Test of getTiemposCazador method, of class Configuracion.
-     */
     @Test
     public void testGetTiemposCazador() {
         System.out.println("getTiemposCazador");
@@ -32,9 +17,6 @@ public class ConfiguracionTest {
         assertEquals(expResult, result);
     }
 
-    /**
-     * Test of getTiempoMuerto method, of class Configuracion.
-     */
     @Test
     public void testGetTiempoMuerto() {
         System.out.println("getTiempoMuerto");
@@ -44,16 +26,13 @@ public class ConfiguracionTest {
         assertEquals(expResult, result);
     }
 
-    /**
-     * Test of getTiempoPresa method, of class Configuracion.
-     */
     @Test
     public void testGetTiempoPresa() {
         System.out.println("getTiempoPresa");
         Configuracion instance = Configuracion.getConfiguracion();
-         Long expResult = (long) 1000;
+        Long expResult = (long) 1000;
         Long result = instance.getTiempoPresa();
         assertEquals(expResult, result);
     }
-    
+
 }
