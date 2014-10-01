3fcd87cdf3692f3637d4f93b4ce2b15c4156e817
jaschu <jaschu@jaschu-VPCYB15AL>
2014-10-01 17:18:25 -0300

    Implementacion de getStrategy de StrategySearcher

--- 7510201402g1/src/main/java/com/tdd/model/strategyFactory/StrategySearcherFactory.java
+++ 7510201402g1/src/main/java/com/tdd/model/strategyFactory/StrategySearcherFactory.java
@@ -1,5 +1,16 @@
 package com.tdd.model.strategyFactory;
 
+import com.tdd.model.ghost.Strategy;
+import com.tdd.model.strategy.StrategySearcher;
+
 public class StrategySearcherFactory implements StrategyFactory{
+
+    /**
+     * @return Devuelve una instancia de la estrategia StrategySearcher
+     */
+    @Override
+    public Strategy getStrategy(){
+        return new StrategySearcher();
+    }
 
 }
