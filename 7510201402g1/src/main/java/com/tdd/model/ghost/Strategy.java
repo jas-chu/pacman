ca3f2efd4a3b4e4c731f2482531d27bfe61c076e
jaschu <jaschu@jaschu-VPCYB15AL>
2014-10-08 01:28:50 -0300

    Metodos a implementar para buscar direccion en Strategy y/o en cada estrategia en particular

--- 7510201402g1/src/main/java/com/tdd/model/ghost/Strategy.java
+++ 7510201402g1/src/main/java/com/tdd/model/ghost/Strategy.java
@@ -4,15 +4,30 @@ import com.tdd.model.stageAbstractions.Direction;
 import com.tdd.model.stageAbstractions.Enemy;
 
 public abstract class Strategy {
-	
-	protected Enemy enemy;
-	
-	public Strategy(Enemy givenEnemy) {
-		this.enemy = givenEnemy;
-	}
-	
-	public abstract Direction getDirection();
-	
-	public abstract void advanceCycle();
-	
+
+    protected Enemy enemy;
+
+    public Strategy(Enemy givenEnemy) {
+        this.enemy = givenEnemy;
+    }
+
+    public Direction getDirection(){
+        throw new UnsupportedOperationException("Not supported yet.");
+//        buscar pacman en area:
+//	si no lo ve:
+//		se mueve random o va a la ultima posicion (uno u otro par de estrategias(*))
+//	si lo ve:
+//		lo persigue(en comun para los 4)
+
+    }
+
+    public abstract void advanceCycle();
+
+    // ojo: no es random random sino que tiene logica de seguir canales del laberinto.
+    // y solo es random en los cruces.
+    public abstract Direction getRandomDirection();
+    
+    public abstract Direction chasePacman();
+        
+
 }
