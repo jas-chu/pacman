8708f5aafcd8ed197b569cdf6323b28209806da5
Daniel E. Fernandez <daniel@chopin>
2014-10-16 20:16:17 -0300

    Separacion de configuracion de paths

--- 7510201402g1/src/main/java/com/tdd/application/main/Application.java
+++ 7510201402g1/src/main/java/com/tdd/application/main/Application.java
@@ -17,17 +17,11 @@ public abstract class Application {
 	public Application() {
 		this.constants = new SpanishXMLConstants();
 		this.configs = new GameConfigurations(constants);
-		this.configureProgram(configs);
+		this.configureProgram();
 	}
 	
-	private void configureProgram(GameConfigurations configs) {
-		String rootPath = System.getProperty("user.dir") + File.separator + "doc";
-		rootPath += File.separator + "xmls" + File.separator + "xmls" + File.separator;
-		
-		configs.XMLStagePath = rootPath + "laberintos" + File.separator + "LaberintoSimple.xml";
-		configs.XMLCharactersPath = rootPath + "laberintos" + File.separator + "PersonajesSimple.xml";
-		configs.XMLPacmanMovementDirectory = rootPath + XMLConstants.PACMAN;
-		configs.XMLSerializationPath = System.getProperty("user.dir")+ File.separator + "ciclosJuego";
+	private void configureProgram() {
+		this.configurePaths();
 		
 		configs.ghostAngerWaitingCycles = new ArrayList<Long>();
 		configs.ghostAngerWaitingCycles.add(new Long(5000));
@@ -38,6 +32,16 @@ public abstract class Application {
 		configs.ghostPreyWaitingCycles = 350;
 		configs.ghostVision = 4;
 		configs.ghostIncrementalVision = 1;
+	}
+	
+	protected void configurePaths() {
+		String rootPath = System.getProperty("user.dir") + File.separator + "doc";
+		rootPath += File.separator + "xmls" + File.separator + "xmls" + File.separator;
+		
+		configs.XMLStagePath = rootPath + "laberintos" + File.separator + "LaberintoSimple.xml";
+		configs.XMLCharactersPath = rootPath + "laberintos" + File.separator + "PersonajesSimple.xml";
+		configs.XMLPacmanMovementDirectory = rootPath + XMLConstants.PACMAN;
+		configs.XMLSerializationPath = System.getProperty("user.dir")+ File.separator + "ciclosJuego";
 	}
 	
 	protected abstract void createGame() throws MalformedXMLException;
