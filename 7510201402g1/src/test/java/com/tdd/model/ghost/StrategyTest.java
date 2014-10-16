08d36d8d3725f4ceb200f1ac2feee918a1ccdd8a
jaschu <jaschu@jaschu-VPCYB15AL>
2014-10-15 22:04:08 -0300

    Creacion de Test de Estrategias

---
+++ 7510201402g1/src/test/java/com/tdd/model/ghost/StrategyTest.java
@@ -0,0 +1,56 @@
+package com.tdd.model.ghost;
+
+import com.tdd.model.stageAbstractions.Direction;
+import com.tdd.model.stageAbstractions.Position;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StrategyTest {
+    
+    public StrategyTest() {
+    }
+/*
+    @Test
+    public void testAdvanceCycle() {
+        System.out.println("advanceCycle");
+        Strategy instance = null;
+        instance.advanceCycle();
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetDirection() {
+        System.out.println("getDirection");
+        Strategy instance = null;
+        Direction expResult = null;
+        Direction result = instance.getDirection();
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetPossibleDirections() {
+        System.out.println("getPossibleDirections");
+        Strategy instance = null;
+        instance.getPossibleDirections();
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testChasePacman() {
+        System.out.println("chasePacman");
+        Position givenPosition = null;
+        Strategy instance = null;
+        instance.chasePacman(givenPosition);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetRandomDirection() {
+        System.out.println("getRandomDirection");
+        Strategy instance = null;
+        instance.getRandomDirection();
+        fail("The test case is a prototype.");
+    }
+*/
+}
---
+++ 7510201402g1/src/test/java/com/tdd/model/strategy/LazyStrategyTest.java
@@ -0,0 +1,16 @@
+package com.tdd.model.strategy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LazyStrategyTest {
+    
+    public LazyStrategyTest() {
+    }
+
+//    @Test
+//    public void getPossibleDirections() {
+//        fail("The test case is a prototype.");
+//    }
+    
+}
