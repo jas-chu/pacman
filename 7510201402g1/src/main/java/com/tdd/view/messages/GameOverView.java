d8e1a802e34763135d5f4ebf12167f8475a0857c
jaschu <jaschu@jaschu-VPCYB15AL>
2014-12-04 16:07:32 -0300

    Creo niveles para mostrar

--- 7510201402g1/src/main/java/com/tdd/view/messages/GameOverView.java
+++ 7510201402g1/src/main/java/com/tdd/view/messages/GameOverView.java
@@ -13,14 +13,13 @@ public class GameOverView extends Message {
     public GameOverView() {
         super();
         this.setGameOvertLayout();
-        this.setText("GAME OVER!!!");
+        this.setText("GAME OVER!!!");        
         
     }
 
-    private void setGameOvertLayout() {
-        this.setFont(new Font("Arial", 0, 40));
-        this.setForeground(Color.blue);
-        this.setBounds(150, 200, ViewConstants.LABEL_WIDTH, ViewConstants.LABEL_HEIGHT);
-
+    private void setGameOvertLayout() {        
+        this.setFont(new Font("Consola", 40, 60));
+        this.setForeground(Color.red);
+        this.setBounds(150, 200, ViewConstants.LABEL_WIDTH, ViewConstants.LABEL_HEIGHT);    
     }
 }
